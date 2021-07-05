238c352412b2edd9a005447c51037649aff3820c
kish02 <kishankumar1515@gmail.com>
2021-07-05 15:26:12 +0530

    commit

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/profile.jpeg";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Patwa Kishankumar Srirajkumar</strong>
+                <br />A Highly motivated Web Developer. I am a Full Stack Web Developer with React.js, Node.js, and MongoDB as my tech stack.
+                <br />
+                I am pursuing my Engineering from Bhilai Institute of Technology, Durg in Computer Science.
+                <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://drive.google.com/file/d/1gZn1talKtP0JuQPqDK2Kx7r4edbbSets/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/kish02" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/kishan-kumar-23a786168/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
